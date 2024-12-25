d73e1588c97d16b5bde94d114d47013e9389c513
DeekshithReddyA <deekshithreddy351@gmail.com>
2024-12-25 23:09:18 +0530

    Added backend url

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       epochsLr,
     };
     setData(updatedData);
-    const response = await axios.post('http://localhost:4000/train', updatedData);
+    const response = await axios.post('https://neural-network-visualizer.onrender.com/train', updatedData);
     setLossResponse({ metric_name: response.data.metric_name, loss: response.data.loss });
     console.log(response.data);
   };
